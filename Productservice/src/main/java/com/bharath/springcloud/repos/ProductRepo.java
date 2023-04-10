18e8d1680d6a15c9383485f97fd264a335e3a0a3
parvez Ahmad <parvezi9ahmad@gmail.com>
2023-04-10 15:11:37 +0530

    summary and postman collection

--- Productservice/src/main/java/com/bharath/springcloud/repos/ProductRepo.java
+++
@@ -1,9 +0,0 @@
-package com.bharath.springcloud.repos;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.bharath.springcloud.Entity.Product;
-
-public interface ProductRepo extends JpaRepository<Product, Long>{
-
-}
